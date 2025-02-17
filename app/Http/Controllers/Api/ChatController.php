bfa2c686681f0461b61d366ce9f92c369e5e197a
Lucas Antonio <lucasasantosbarbosa@gmail.com>
2025-02-17 18:44:23 -0300

    feat: Adicionando documentação openapi no projeto para disponibilizar para os clientes

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Interfaces\WhatsappServiceInterface;
+use App\Services\Internal\Whatsapp\WhatsappManagerService;
 
 class ChatController extends Controller
 {
-    public function __construct(protected WhatsappServiceInterface $whatsappService ){}
+    public function __construct(protected WhatsappManagerService $whatsappService ){}
 
     public function checkNumberWhatsapp($phone)
     {
--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Interfaces\WhatsappServiceInterface;
+use App\Services\Internal\Whatsapp\WhatsappManagerService;
 use Illuminate\Http\Request;
+use Knuckles\Scribe\Attributes\Authenticated;
+use Knuckles\Scribe\Attributes\BodyParam;
+use Knuckles\Scribe\Attributes\Endpoint;
+use Knuckles\Scribe\Attributes\Group;
 
+#[Group("Mensagens", "Todos endpoints de mensagens")]
 class MessageController extends Controller
 {
-    public function __construct(protected WhatsappServiceInterface $whatsappService){}
+    public function __construct(protected WhatsappManagerService $whatsappService){}
     
+    #[Endpoint("Enviar mensagem", <<<DESC
+        Endpoint para enviar mensagem pelo Whatsapp.
+    DESC)]
+    #[BodyParam("phone", "string", required: true, example: "55000000000")]
+    #[BodyParam("message", "string", required: true, example: "Hello world!")]
+    #[Authenticated]
     public function sendMessage(Request $request)
     {
-        $response = $this->whatsappService::sendMessage($request->instance->name, $request->phone, $request->message);
+        $response = $this->whatsappService->sendMessage($request->instance->name, $request->phone, $request->message);
         
         return $response;
     }
